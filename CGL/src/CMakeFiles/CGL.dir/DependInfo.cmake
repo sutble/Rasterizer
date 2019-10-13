d77f63c21984fd06df18b5466a2aa3d7e28340fa
Anika Garg <anikagarg02@gmail.com>
2019-10-13 14:40:32 -0700

    Importing

---
+++ CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/base64.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/base64.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/color.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/color.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/complex.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/complex.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/lodepng.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/lodepng.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/matrix3x3.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/matrix3x3.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/matrix4x4.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/matrix4x4.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/osdfont.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/osdfont.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/osdtext.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/osdtext.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/path.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/path.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/quaternion.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/quaternion.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/spectrum.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/spectrum.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/tinyxml2.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/tinyxml2.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/vector2D.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/vector2D.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/vector3D.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/vector3D.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/vector4D.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/vector4D.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/viewer.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/viewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "CGL/include"
+  "CGL/deps/glew/include"
+  "/usr/include/freetype2"
+  "CGL/include/CGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/draw.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/src/drawrend.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/src/CMakeFiles/draw.dir/drawrend.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/src/main.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/src/CMakeFiles/draw.dir/main.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/src/svg.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/src/CMakeFiles/draw.dir/svg.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/src/svgparser.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/src/CMakeFiles/draw.dir/svgparser.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/src/texture.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/src/CMakeFiles/draw.dir/texture.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/src/transforms.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/src/CMakeFiles/draw.dir/transforms.cpp.o"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/src/triangulation.cpp" "/home/aditya/School/CS184/p1-rasterizer-sutble/src/CMakeFiles/draw.dir/triangulation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "CGL/include"
+  "/usr/include/freetype2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  "/home/aditya/School/CS184/p1-rasterizer-sutble/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
